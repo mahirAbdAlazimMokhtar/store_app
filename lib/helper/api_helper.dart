f13d90d17f59697e2ef0221b0a8b38f4cc9da68d
Mahir Molhtar <mahr.bwb@gmail.com>
2022-07-04 11:08:28 +0300

    add post request to api

--- lib/helper/api_helper.dart
+++ lib/helper/api_helper.dart
@@ -1,16 +1,35 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
   Future<dynamic> get({required String url}) async {
     http.Response response = await http.get(Uri.parse(url));
-    
+
     if (response.statusCode == 200) {
-      return  jsonDecode(response.body);
-    }else{
+      return jsonDecode(response.body);
+    } else {
       throw Exception('there was an error in your code ${response.statusCode}');
     }
-    
+  }
+
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll(
+        {
+          'Authorization': 'Bearer$token',
+        },
+      );
+    }
+    http.Response response =
+        await http.post(Uri.parse(url), body: body, headers: headers);
+
+    Map<String, dynamic> data = jsonDecode(response.body);
+    return data;
   }
 }
